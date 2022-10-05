c7fae19d29664714134c8086a659184628c6debf
Aleksandr <sanyogromov@mail.ru>
2022-10-05 21:07:34 +0300

    update 05.10

--- app/src/main/java/com/example/citydirectory/AddOrg.java
+++ app/src/main/java/com/example/citydirectory/AddOrg.java
@@ -1,14 +1,60 @@
 package com.example.citydirectory;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import java.sql.Connection;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.sql.Statement;
 
 public class AddOrg extends AppCompatActivity {
+
+
+    Connection connection;
+    String errorMessage = "";
+
+    EditText ID;
+    EditText Name;
+    EditText Place;
+    EditText Number;
+    EditText Mail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_org);
+
+        ID = findViewById(R.id.IdOrgAdd);
+        Name = findViewById(R.id.nameOrgAdd);
+        Place = findViewById(R.id.placeOrgAdd);
+        Number = findViewById(R.id.numberOrgAdd);
+        Mail = findViewById(R.id.mailOrgAdd);
+    }
+
+    public void inputSQL(View v) {
+        TextView ID = findViewById(R.id.IdOrgAdd);
+        TextView Name = findViewById(R.id.nameOrgAdd);
+        TextView Place = findViewById(R.id.placeOrgAdd);
+        TextView Number = findViewById(R.id.numberOrgAdd);
+        TextView Mail = findViewById(R.id.mailOrgAdd);
+        try {
+
+            ConnectionHelper ch = new ConnectionHelper();
+            connection = ch.connectionClass();
+            if (connection != null) {
+                String query = "Insert into Organizations(ID, Name, Place, Number, Mail)" +
+                        " Values('" + ID.getText() + "', '" + Name.getText() + "', '" + Place.getText() + "', '" + Number.getText() + "', '" + Mail.getText() + "')";
+                Statement statement = connection.createStatement();
+                statement.executeUpdate(query);
+                Toast.makeText(this, "Организация добавлена!", Toast.LENGTH_LONG).show();
+
+            } else {
+                errorMessage = "Попробуйте еще раз.";
+            }
+        } catch (Exception ex) {
+            errorMessage = "Check Connection!";
+        }
     }
 }
--- app/src/main/java/com/example/citydirectory/MainActivity.java
+++ app/src/main/java/com/example/citydirectory/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.citydirectory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+import javax.sql.RowSet;
 
 public class MainActivity extends AppCompatActivity {
     Connection connection;
@@ -21,13 +27,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void GetTextFromSQL(View v){
-        TextView ID = findViewById(R.id.IDorg);
-        TextView Name = findViewById(R.id.NAMEorg);
-        TextView Place = findViewById(R.id.PLACEorg);
-        TextView Number = findViewById(R.id.NUMBERorg);
-        TextView Mail = findViewById(R.id.MAILorg);
-
-        Button List = findViewById(R.id.buttonNext);
+        TableLayout listen = findViewById(R.id.listen);
+        listen.removeAllViews();
         Button Add = findViewById(R.id.buttonAdd);
 
         try{
@@ -38,19 +39,35 @@ public class MainActivity extends AppCompatActivity {
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
                 while(resultSet.next()){
-                    ID.setText(resultSet.getString(1));
-                    Name.setText(resultSet.getString(2));
-                    Place.setText(resultSet.getString(3));
-                    Number.setText(resultSet.getString(4));
-                    Mail.setText(resultSet.getString(5));
+                    TableRow dbOutputRow = new TableRow(this);
+                    dbOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT));
+                    TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                            TableRow.LayoutParams.WRAP_CONTENT);
+
+                    TextView NAME_ORG = new TextView(this);
+                    params.weight = 1.0f;
+                    NAME_ORG.setLayoutParams(params);
+                    NAME_ORG.setText(resultSet.getString(2));
+                    NAME_ORG.setTextSize(12);
+                    dbOutputRow.addView(NAME_ORG);
+
+                    TextView NUMBER_ORG = new TextView(this);
+                    params.weight = 1.0f;
+                    NUMBER_ORG.setLayoutParams(params);
+                    NUMBER_ORG.setText(resultSet.getString(4));
+                    NUMBER_ORG.setTextSize(12);
+                    dbOutputRow.addView(NUMBER_ORG);
+
+                    TextView MAIL_ORG = new TextView(this);
+                    params.weight = 1.0f;
+                    MAIL_ORG.setLayoutParams(params);
+                    MAIL_ORG.setText(resultSet.getString(5));
+                    MAIL_ORG.setTextSize(12);
+                    dbOutputRow.addView(MAIL_ORG);
+
+                    listen.addView(dbOutputRow);
                 }
-                List.setVisibility(View.GONE);
-                Add.setVisibility(View.VISIBLE);
-                ID.setVisibility(View.VISIBLE);
-                Name.setVisibility(View.VISIBLE);
-                Place.setVisibility(View.VISIBLE);
-                Number.setVisibility(View.VISIBLE);
-                Mail.setVisibility(View.VISIBLE);
             }
             else{
                 ConnectionResult = "Check Connection!";
